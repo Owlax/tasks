b006f6bb1c16ff30b775b4bb86d9c893c2e89a85
Alex <acroce@udel.edu>
2023-03-31 22:38:04 -0400

    CheckAnswer implimented and passing tests

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
+import { Col, Form, Row } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+    function checkAnswer(event: React.ChangeEvent<HTMLTextAreaElement>) {
+        setAnswer(event.target.value);
+    }
     return (
         <div>
-            <h3>Check Answer</h3>
+            <Form.Group controlId="formAnswer" as={Row}>
+                <Form.Label column sm={2}>
+                    Check Answer
+                </Form.Label>
+                <Col>
+                    <Form.Control
+                        value={answer}
+                        onChange={checkAnswer}
+                        type="answer"
+                        placeholder="Enter answer"
+                    />
+                </Col>
+                <Col>{answer === expectedAnswer ? "✔️" : "❌"}</Col>
+            </Form.Group>
         </div>
     );
 }
